794e234bb329cfd7ee54404dcdb81a2bb142348e
91976 <91976@192.168.1.36>
2025-01-01 17:45:14 +0530

    update testng files

--- src/test/java/testpackage/test1.java
+++ src/test/java/testpackage/test1.java
@@ -8,20 +8,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
-import org.openqa.selenium.*;
 import org.testng.Assert;
-import org.testng.annotations.*;
 
 public class test1 {
 	WebDriver driver ;
 	@Test
 	public void f() {
 	  
-	    String baseUrl = "https://www.toolsqa.com/";
-	      
-	         
+			String baseUrl = "https://www.toolsqa.com/";         
 	        System.out.println("Launching Google Chrome browser"); 
-	        driver = new ChromeDriver();
 	        driver.get(baseUrl);
 	        String testTitle = "Tools QA";
 	        String originalTitle = driver.getTitle();
@@ -32,12 +27,14 @@ public class test1 {
 	public void beforeMethod() {
 		System.out.println("Starting Test On Chrome Browser");
 		WebDriverManager.chromedriver().setup();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\91976\\Downloads\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
 
 	}
 	
 	@AfterMethod
 	public void afterMethod() {
-		 driver.close();
+		driver.close();
 		System.out.println("Finished Test On Chrome Browser");
 	}
 }
